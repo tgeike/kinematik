788fee15b097b72d7f36fbfbfb06e88c7230fe2d
T Geike <132821768+tgeike@users.noreply.github.com>
2025-10-16 14:05:47 +0200

    Dummy hochgeladen

---
+++ KM116_konst-Beschl.jl
@@ -0,0 +1,35 @@
+### A Pluto.jl notebook ###
+# v0.20.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2ed12ce0-aa88-11f0-0667-758927ba9cfe
+md"""
+# Kinematik mit Julia
+## Geradlinige Bewegung
+
+!!! info "Inhalt folgt"
+    Die Aufgabe ist noch nicht ausgearbeitet.
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.11.5"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─2ed12ce0-aa88-11f0-0667-758927ba9cfe
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
